8a70245188609c73b8d43e35199561db06e1d7d3
admin <admin@admins-iMac.local>
2018-08-30 12:08:15 +0300

    fix path to TabNavigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export { default as BarPanel } from './BarPanel';
 export { default as TabButton } from './TabButton';
-export { default as TabNavigation } from '.TabNavigation';
+export { default as TabNavigation } from './TabNavigation';
